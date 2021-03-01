3fbb33b39988e2322bca5c16993b3fdd68e8125f
Abraão Pinto <abraao.pinto@bancoamazonia.com.br>
2021-03-01 17:25:35 -0300

    change: + fix the import + fix const name useful - remove spaces

--- test/string-functions.spec.js
+++ test/string-functions.spec.js
@@ -1,38 +1,36 @@
 const chai = require('chai')
 const expect = chai.expect
-const stringUseful = require('../src/string-functions/index')
-
-
+const useful  = require('../index')
 
 describe('Function capitalize', () => {
     const defaultReturn = ''
     it('test in the "capitalize" function with plain text.', () => {
         const textCapitalize = 'TEXT CAPITALIZE'
-        expect(stringUseful.capitalize('text capitalize')).to.equal(textCapitalize)
+        expect(useful.capitalize('text capitalize')).to.equal(textCapitalize)
     })
     it('test in the "capitalize" function with null text.', () => {
         const nullText = null
-        expect(stringUseful.capitalize(nullText)).to.equal(defaultReturn)
+        expect(useful.capitalize(nullText)).to.equal(defaultReturn)
     })
     it('test in the "capitalize" function with text undefined.', () => {
         const undefinedText = undefined
-        expect(stringUseful.capitalize(undefinedText)).to.equal(defaultReturn)
+        expect(useful.capitalize(undefinedText)).to.equal(defaultReturn)
     })
     it('test in the "capitalize" function with text with special characters.', () => {
         const textSpecialCaracteresCapitalize = 'TEXT WITH SPECIAL CHARACTERS !@#$%¨&*()_+ CAPITALIZE'
-        expect(stringUseful.capitalize('text with special characters !@#$%¨&*()_+ capitalize')).to.equal(textSpecialCaracteresCapitalize)
+        expect(useful.capitalize('text with special characters !@#$%¨&*()_+ capitalize')).to.equal(textSpecialCaracteresCapitalize)
     })
     it('test in the "capitalize" function with object.', () => {
         const object = {}
-        expect(stringUseful.capitalize(object)).to.equal(defaultReturn)
+        expect(useful.capitalize(object)).to.equal(defaultReturn)
     })
     it('test in the "capitalize" function with function.', () => {
         const functionTest = () => 'null'
-        expect(stringUseful.capitalize(functionTest)).to.equal(defaultReturn)
+        expect(useful.capitalize(functionTest)).to.equal(defaultReturn)
     })
     it('test in the "capitalize" function with number.', () => {
         const testNumber = 1984
-        expect(stringUseful.capitalize(testNumber)).to.equal(defaultReturn)
+        expect(useful.capitalize(testNumber)).to.equal(defaultReturn)
     })
 })
 
