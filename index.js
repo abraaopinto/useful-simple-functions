58ce0602ff461b0e40afe5418751f71087ff1cc1
Abraão Pinto <abraao.pinto@bancoamazonia.com.br>
2021-03-02 16:32:19 -0300

    chg: +code refactory +configure new functions

--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-module.exports = require('./src/string-functions/index')
+module.exports = require('./src')
---
+++ src/index.js
@@ -0,0 +1,11 @@
+const all = require('./all-functions')
+const string = require('./string-functions')
+
+const useful = {
+    ...all,
+    ...string
+}
+
+module.exports = useful
+
+module.exports.default = useful
