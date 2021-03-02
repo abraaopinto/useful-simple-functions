cad9b6908fbbe137f160c354017d0ee035a89c88
Abraão Pinto <abraao.pinto@bancoamazonia.com.br>
2021-03-02 16:29:04 -0300

    add new functions

---
+++ src/all-functions/index.js
@@ -0,0 +1,21 @@
+const isNull = o => o === null || typeof o === undefined 
+const isNotNull = o => !isNull(o)
+const isEmpty = function (o) {  
+    if (isNull(o)) return true
+
+    if (o.constructor === Object) return  Object.entries(o).length === 0
+
+    if (o.constructor === Array || o.constructor === String) return !o.length
+
+    if (o.constructor === Set || o.constructor === Map) return !o.size
+
+    return true
+}
+const isNotEmpty = o => !isEmpty(o)
+
+module.exports = {
+    isNull,
+    isNotNull,
+    isEmpty,
+    isNotEmpty
+}
