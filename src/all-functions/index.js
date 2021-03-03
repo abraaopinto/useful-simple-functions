532cde2d9b7fb66451e2f9f0b1d36fd0fb871eff
Abraão Pinto <abraao.pinto@bancoamazonia.com.br>
2021-03-03 11:19:26 -0300

    changes: + refactoring the code - removing blank spaces and lines

--- src/all-functions/index.js
+++ src/all-functions/index.js
@@ -1,20 +1,14 @@
-const isNull = o => o === null || typeof o === undefined 
-const isNotNull = o => !isNull(o)
-const isEmpty = function (o) {  
+const isNull = o => !o
+const isNotNull = o => !!o
+const isEmpty = o => {  
     if (isNull(o)) return true
-
     if (o.constructor === Object) return  Object.entries(o).length === 0
-
     if (o.constructor === Array || o.constructor === String) return o.length === 0
-
     if (o.constructor === Set || o.constructor === Map) return o.size === 0
-
     return true
 }
 const isNotEmpty = o => !isEmpty(o)
-
 const isBlank = s => isNotEmpty(s) && s.constructor === String && s.trim().length === 0
-
 module.exports = {
     isNull,
     isNotNull,
