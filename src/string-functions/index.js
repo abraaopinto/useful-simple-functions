4d1ff7dfe8ea8f7bcde6f4886eb6cad7c702b57f
Abraão Pinto <abraao.pinto@bancoamazonia.com.br>
2021-03-09 16:14:51 -0300

    chages: + new function reverse text

--- src/string-functions/index.js
+++ src/string-functions/index.js
@@ -2,11 +2,13 @@ const sanitize = s => (typeof s !== 'string') ? '' : s.toString().replace(/[^A-Z
 const capitalize = s => (typeof s !== 'string') ? '' : s.toUpperCase()
 const sanitizeAndCapitalize = s => (typeof s !== 'string') ? '' : capitalize(sanitize(s))
 const capitalizeFirst = s => (typeof s !== 'string') ? '' : capitalize(s.charAt(0)) + s.slice(1)
+const reverse = s => ( typeof s !== 'string') ? '' : s.split('').reverse().join('')
 const formatReverseDate = d => (typeof d !== 'string') ? '' : d.split(/\D/).reverse().join('-')
 module.exports = {
   sanitize,
   capitalize,
   sanitizeAndCapitalize,
   capitalizeFirst,
+  reverse,  
   formatReverseDate
 }
